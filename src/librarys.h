e434e2ae3e60848eacab4ed11a884275d14aae95
muhammad6to9 <adm118331@gmail.com>
2025-07-01 09:47:31 +0300

    Add files via upload

---
+++ src/librarys.h
@@ -0,0 +1,27 @@
+#ifndef s6
+#define s6
+#include <iostream> 
+#include <cmath> 
+#include <math.h> 
+#include <string> 
+#include <random> 
+#include <typeinfo> 
+#include <vector> 
+#include <limits> 
+#include <cstdlib> 
+#include <ctime> 
+#include <unistd.h>
+#include <cstdio>
+#include <iomanip>
+#include <atomic>
+#include <thread>
+#include <fstream>
+#include <algorithm>
+#include <chrono>
+#include <conio.h>
+#include <sstream>
+#include <cstdint>
+#include <cstring>
+#include "inttostring.h"
+#endif
+//yeah nerd, i know it's spelled libraries
